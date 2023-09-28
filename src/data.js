79f45c2788c2b1c7ef44f369bf3254804964fb28
swetamishra123 <swetamishra603@gmail.com>
2023-09-28 21:07:27 +0530

    adding

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Sweta Mishra",
+    job: "Software Engineer",
+    image: "https://media.licdn.com/dms/image/D4D35AQGp4CPNeqwHTQ/profile-framedphoto-shrink_400_400/0/1686409473658?e=1696521600&v=beta&t=WOWSwnI0OdPA0BCVoI44BhP8w7V9YKxXhQrEuMs8uK8",
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 2,
+    name: "Demisa George",
+    job: "Graphic Designer",
+    image: "https://politics.princeton.edu/people",
+    text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },
+  {
+    id: 3,
+    name: "Danish Xeria",
+    job: "Marketing Manager",
+    image: "https://aeccc.targettechnology.in/static/media/SoumyaBanerjee.2e2521d602https://www.rri.res.in/people",
+    text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+  },
+  {
+    id: 4,
+    name: "Prabhu saras",
+    job: "Content Strategist",
+    image: "https://www.cnbc.com/2023/04/25/ramit-sethi-best-advice-for-people-in-their-20s-to-build-wealth.html",
+    text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+  },
+  {
+    id: 5,
+    name: "Verusean",
+    job: "Data Analyst",
+    image: "https://www.globenewswire.com/en/news-release/2022/02/09/2381921/0/en/Verusen-Appoints-Addy-Robinson-Head-of-People-Operations-Continues-Forward-Growth-After-25-Million-Series-B-Funding.html",
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+];
+
+export default reviews;
