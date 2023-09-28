46b41e7942847c11a3986be35e8f312a0725a169
Sweta Mishra <85573172+swetamishra123@users.noreply.github.com>
2023-09-29 00:23:30 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,7 +9,8 @@ const Card = (props) => {
       <div className='absolute top-[-7rem] z-[10] mx-auto'>
         <img 
         className="aspect-square rounded-full w-[140px] h-[140px] z-[25]"
-        src={review.image}/>
+        src={review.image}
+    alt={review.name}/>
         <div className='w-[140px] h-[140px] bg-violet-500 rounded-full absolute
          top-[-6px] z-[-10] left-[10px]'></div>
       </div>
